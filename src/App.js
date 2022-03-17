77d867e8491b75ed3b64ffc6a9b05cabff8cf2b9
masonsteeger94 <masonsteeger@gmail.com>
2022-03-17 00:14:26 -0500

    completed intro animation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [ovFlow, setOvFlow] = useState("hidden");
+  const [bgCol, setBgCol] = useState("#282c34");
+  const [animation, setAnimation] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      console.log("doin it");
+      setOvFlow("visible");
+      setBgCol("white");
+    }, 2300);
+    // setTimeout(() => {
+    //   setOvFlow("hidden");
+    // }, 2800);
+    setTimeout(() => {
+      setAnimation(false);
+    }, 3200);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App' style={{ overflow: "hidden" }}>
+      {animation ? (
+        <header className='App-header' style={{ backgroundColor: bgCol }}>
+          <div className='div-box' id='tl'></div>
+          <div className='div-box' id='tr'></div>
+          <div className='div-box' id='bl'></div>
+          <div className='div-box' id='br'></div>
+          <div className='circle-path'>
+            <div className='circle-container'>
+              <div id='halfclip-left' style={{ overflow: ovFlow }}>
+                <div class='halfcircle' id='clipped'></div>
+              </div>
+              <div id='halfclip-right' style={{ overflow: ovFlow }}>
+                <div class='halfcircle' id='clipped'></div>
+              </div>
+            </div>
+          </div>
+        </header>
+      ) : (
+        <div className='main'>
+          <h1>Coming soon!</h1>
+        </div>
+      )}
     </div>
   );
 }
