5b0cf7d6ceabaf8faf6da385ceea20d734bccc91
masonsteeger94 <masonsteeger@gmail.com>
2022-04-02 22:48:46 -0500

    added stack icons and updated home page

--- src/DesktopPages/DesktopHome.js
+++ src/DesktopPages/DesktopHome.js
@@ -4,10 +4,23 @@ const DesktopHome = ({ data }) => {
   return (
     <div className='desktop-home-container'>
       <div className='title-container'>
-        <h1>Mason Steeger</h1>
-        <h2>software developer</h2>
-        <img src={data.meta.mainLogo} height='250px' alt='logo' />
-        <h2>Desktop site coming soon!</h2>
+        <h1 id='desktop-name'>Mason Steeger</h1>
+        <div className='subtitle-container'>
+          <h2 id='desktop-subtitle'>software developer</h2>
+          <div className='stack-logo-container'>
+            {data.meta.techStack.map((tech, i) => {
+              return (
+                <img
+                  className='stack-logo'
+                  src={`/icons/stack/${tech}.svg`}
+                  alt={tech}
+                />
+              );
+            })}
+          </div>
+        </div>
+
+        {/* <img src={data.meta.mainLogo} height='500px' alt='logo' /> */}
       </div>
     </div>
   );
